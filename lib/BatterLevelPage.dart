61b4ab45afefbc1045dddff9ae1ded92c5ab85c4
Shang <shang1219178163@gmail.com>
2021-04-24 10:30:31 +0800

    电池电量统计

---
+++ lib/BatterLevelPage.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:styled_widget/styled_widget.dart';
+import 'DDLog.dart';
+import 'navigator_extension.dart';
+
+
+class BatterLevelPage extends StatefulWidget {
+  BatterLevelPage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _BatterLevelPageState createState() => _BatterLevelPageState();
+}
+
+class _BatterLevelPageState extends State<BatterLevelPage> {
+
+  static const platform = const MethodChannel('samples.flutter.io/battery');
+  String _batteryLevel = "Unknown battery level.";
+
+  _getBatterLevel() async{
+    String batteryLevel;
+
+    try {
+      final int result = await platform.invokeMethod('getBatteryLevel');
+      batteryLevel = 'Battery level at $result % .';
+    } on PlatformException catch(e) {
+      batteryLevel = "Failed to get battery level: '${e.message}'.";
+    }
+    setState((){
+      _batteryLevel = batteryLevel;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+          leading: Icon(Icons.arrow_back)
+                    .gestures(onTap: (){
+                  NavigatorExt.popPage(context);
+                  // Navigator.popPage(context);
+                  // if (!Navigator.canPop(context)) {
+                  //   DDLog("已经是根页面了！", StackTrace.current);
+                  //   return;
+                  // }
+                  // Navigator.pop(context);
+                }),
+          // Here we take the value from the BatterLevelPage object that was created by
+        // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              _batteryLevel,
+            ),
+            OutlinedButton.icon(
+              icon: Icon(Icons.search),
+              label: Text("Get Battery Level"),
+              onPressed: _getBatterLevel,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
