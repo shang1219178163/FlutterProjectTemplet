b9c1e99f56aa8f0fe4bfb73cad4f66c5a2288fd7
Shang <shang1219178163@gmail.com>
2021-04-24 10:33:58 +0800

    扩展方法

---
+++ lib/Navigator_extension.dart
@@ -0,0 +1,184 @@
+//
+//
+// ignore: avoid_web_libraries_in_flutter
+// import 'package:flutter/cupertino.dart' show BuildContext, Navigator;
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'DDLog.dart' show DDLog;
+
+extension NavigatorExt on Navigator{
+
+  static void popPage(BuildContext context, [StackTrace? current]){
+    if (!Navigator.canPop(context)) {
+      if (current != null) {
+        DDLog("已经是根页面了！", current);
+      }
+      return;
+    }
+    Navigator.pop(context);
+  }
+}
+
+enum ImageAlignment {
+  /// 文字左边
+  left,
+
+  /// 文字顶部
+  top,
+
+  /// 文字右边
+  right,
+
+  /// 文字底部
+  bottom,
+}
+
+extension TextButtonExt on TextButton{
+
+  // ignore: non_constant_identifier_names, missing_return
+  static TextButton build(String text, Widget? image, ImageAlignment imageAlignment, void callback(String value)) {
+    if (image == null) {
+      return TextButton(
+        onPressed: () => callback(text),
+        child: Text(text),
+      );
+    }
+
+    switch (imageAlignment) {
+      case ImageAlignment.left:
+        return TextButton(
+          onPressed: () => callback(text),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              image,
+              SizedBox(width: 8),
+              Text(text),
+            ],
+          ),
+        );
+
+      case ImageAlignment.top:
+        return TextButton(
+          onPressed: () => callback(text),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              image,
+              SizedBox(width: 8),
+              Text(text),
+            ],
+          ),
+        );
+
+      case ImageAlignment.right:
+        return TextButton(
+          onPressed: () => callback(text),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(text),
+              SizedBox(width: 8),
+              image,
+            ],
+          ),
+        );
+
+      case ImageAlignment.bottom:
+        return TextButton(
+          onPressed: () => callback(text),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(text),
+              SizedBox(width: 8),
+              image,
+            ],
+          ),
+        );
+    }
+  }
+
+}
+
+// extension WidgetExt on Widget{
+//
+//   // ignore: non_constant_identifier_names, missing_return
+//   static Widget buildBtn(String text, Widget image, ImageAlignment imageAlignment) {
+//     if (image == null) {
+//       return Text(text ?? "",);
+//     }
+//
+//     switch (imageAlignment) {
+//       case ImageAlignment.left:
+//         return Row(
+//           crossAxisAlignment: CrossAxisAlignment.center,
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           mainAxisSize: MainAxisSize.min,
+//           children: <Widget>[
+//             image,
+//             SizedBox(width: 8),
+//             Text(text ?? "",),
+//           ],
+//         );
+//       case ImageAlignment.top:
+//         return Column(
+//           crossAxisAlignment: CrossAxisAlignment.center,
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           mainAxisSize: MainAxisSize.min,
+//           children: <Widget>[
+//             image,
+//             SizedBox(width: 8),
+//             Text(text ?? "",),
+//           ],
+//         );
+//       case ImageAlignment.right:
+//         return Row(
+//           crossAxisAlignment: CrossAxisAlignment.center,
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           mainAxisSize: MainAxisSize.min,
+//           children: <Widget>[
+//             Text(text ?? "",),
+//             SizedBox(width: 8),
+//             image,
+//           ],
+//         );
+//       case ImageAlignment.bottom:
+//         return Column(
+//           crossAxisAlignment: CrossAxisAlignment.center,
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           mainAxisSize: MainAxisSize.min,
+//           children: <Widget>[
+//             Text(text ?? "",),
+//             SizedBox(width: 8),
+//             image,
+//           ],
+//         );
+//     }
+//   }
+//
+// }
+
+
+// class OutlinedButton extends ButtonStyleButton {
+//
+//
+// }
+
+
+// extension on TextStyle {
+//
+//   TextStyle colorChain(Color? color){
+//     this.color = color;
+//     return this;
+//   }
+//
+// }
---
+++ lib/PopupMenuButton_extension.dart
@@ -0,0 +1,122 @@
+
+
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+import 'DDLog.dart';
+
+
+extension PopupMenuButtonExt on PopupMenuButton{
+
+  /// itemBuilder: <PopupMenuItem<String>>[]
+  static from({ required List<String> list, void callback(String value)?}) => PopupMenuButton<String>(
+        itemBuilder: (BuildContext context) => list.map((value) => PopupMenuItem(
+          child: Text(value),
+          value: "${list.indexOf(value)}",
+        )).toList(),
+        onSelected: callback);
+
+  /// itemBuilder: <PopupMenuButton<String>>[]
+  static fromJson({required Map<String, String> json, void callback(String value)?}) => PopupMenuButton<String>(
+        itemBuilder: (BuildContext context) => json.keys.map((key) => PopupMenuItem(
+          child: Text(key),
+          value: json[key],
+        )).toList(),
+        onSelected: callback);
+
+  /// itemBuilder: <CheckedPopupMenuItem<String>>[]
+  static fromCheckList({required List<String> list, int checkedIdx = 0, void callback(String value)?}) {
+    return PopupMenuButton<String>(
+        itemBuilder: (BuildContext context) => list.map((value) => CheckedPopupMenuItem<String>(
+          child: Text(value),
+          checked: checkedIdx == list.indexOf(value),
+          value: "${list.indexOf(value)}",
+        )).toList(),
+        onSelected: callback);
+  }
+
+  /// itemBuilder: <CheckedPopupMenuItem<String>>[]
+  static fromCheckJson({required Map<String, String> json, required String checkedString, void Function(String value)? callback}) async => PopupMenuButton<String>(
+        itemBuilder: (BuildContext context) => json.keys.map((key) => CheckedPopupMenuItem<String>(
+          child: Text(key),
+          checked: key == checkedString,
+          value: json[key],
+        )).toList(),
+        onSelected: callback);
+
+  /// itemBuilder: <PopupMenuEntry<String>>[]
+  static fromEntryList({required List<String> list, required int checkedIdx, void callback(String value)?}) {
+    var items = <PopupMenuEntry<String>>[];
+    for (int i = 0; i < list.length; i++) {
+      final String e = list[i];
+      items.add(
+          CheckedPopupMenuItem<String>(
+            child: Text(e),
+            value: e,
+            checked: i == checkedIdx,
+          ));
+      items.add(PopupMenuDivider(height: 1.0));
+    }
+
+    return PopupMenuButton<String>(
+      itemBuilder: (BuildContext context) => items,
+      onSelected: callback,
+    );
+  }
+  /// itemBuilder: <PopupMenuEntry<String>>[]
+  static fromEntryFromJson({required Map<String, String> json, required String checkedString, void Function(String value)? callback}) {
+    var list = <PopupMenuEntry<String>>[];
+    for (final String e in json.keys) {
+      list.add(
+          CheckedPopupMenuItem<String>(
+            child: Text(e),
+            checked: e == checkedString,
+            value: json[e],
+          ));
+      list.add(PopupMenuDivider(height: 1.0));
+    }
+
+    return PopupMenuButton<String>(
+        itemBuilder: (BuildContext context) => list,
+        onSelected: callback,
+    );
+  }
+
+  // /// create itemBuilder: <PopupMenuItem<String>>[]
+  // static List builderFromJson(Map<String, String> json, String checkedString) {
+  //   return json.keys.map((key) => PopupMenuItem<String>(
+  //     child: Text(key),
+  //     value: json[key],
+  //   )).toList();
+  // }
+  //
+  // /// create itemBuilder: <CheckedPopupMenuItem<String>>[]
+  // static List builderCheckFromJson(Map<String, String> json, String checkedString) {
+  //   return json.keys.map((key) => CheckedPopupMenuItem<String>(
+  //     child: Text(key),
+  //     checked: key == checkedString,
+  //     value: json[key],
+  //   )).toList();
+  // }
+  //
+  // /// create itemBuilder: <PopupMenuEntry<String>>[]
+  // static List builderEntryFromJson(Map<String, String> json, String checkedString) {
+  //   var list = <PopupMenuEntry<String>>[];
+  //   for (final String e in json.keys) {
+  //     list.add(
+  //         CheckedPopupMenuItem<String>(
+  //           child: Text(e),
+  //           checked: e == checkedString,
+  //           value: json[e],
+  //         ));
+  //     list.add(PopupMenuDivider(height: 1.0));
+  //   }
+  //   // DDLog(list, StackTrace.current);
+  //   return list;
+  // }
+}
+
+
+
+
