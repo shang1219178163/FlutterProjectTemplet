c7a7aa07f3f108f36cfc229e3d83c7f90a6b3abf
Shang <shang1219178163@gmail.com>
2021-04-24 10:33:06 +0800

    tabbar

---
+++ lib/SecondPage.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+// import 'package:fluttertemplet/navigator_extension.dart';
+import 'package:styled_widget/styled_widget.dart';
+import 'DDLog.dart';
+import 'Navigator_extension.dart';
+
+class SecondPage extends StatelessWidget {
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            leading: BackButton(
+                      color: Colors.white,
+                      onPressed: (){ NavigatorExt.popPage(context, StackTrace.current); }
+                      ),
+            // .gestures(onTap: ()=> DDLog("back", StackTrace.current)
+            title: Text("$this"),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Text(
+                  'You have pushed the button this many times:',
+                  style: Theme.of(context).textTheme.bodyText1,
+                ).padding(left: 15, right: 15),
+
+                ElevatedButton.icon(
+                    icon: Icon(Icons.send),
+                    label: Text("发送"),
+                    // onPressed: () => DDLog('pressed', StackTrace.current),
+                    onPressed: () => {
+                      test()
+                  },
+                ),
+
+                OutlinedButton.icon(
+                  icon: Icon(Icons.add),
+                  label: Text("添加"),
+                  onPressed: () => DDLog('pressed', StackTrace.current),
+                ),
+
+                TextButton(
+                  onPressed: () => DDLog('$this', StackTrace.current),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text('呼叫', style: TextStyle(color: Theme.of(context).accentColor))
+                          .padding(right: 5),
+                      // SizedBox(width: 30),
+                      Icon(Icons.call),
+                    ],
+                  )
+                  .decorated(color: Colors.yellow, borderRadius: BorderRadius.all(Radius.circular(0))),
+                )
+                    .decorated(color: Colors.green, borderRadius: BorderRadius.all(Radius.circular(5))),
+
+                TextButton(
+                  onPressed: () => DDLog('Make a Note', StackTrace.current),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text("Make a Note"),
+                      SizedBox(width: 5),
+                      Icon(Icons.send),
+                    ],
+                  ),
+                ).decorated(color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(5)),
+                    border: Border.all(width: 1.0, color: Colors.red)),
+
+                // WidgetExt.buildBtn("菜单", Icon(Icons.send), ImageAlignment.right),
+                TextButtonExt.build("菜单left", Icon(Icons.info), ImageAlignment.left, (value) {
+                  DDLog(value, StackTrace.current);
+                }).decorated(color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(5)),
+                    border: Border.all(width: 1.0, color: Colors.red),),
+
+                TextButtonExt.build("菜单right", Icon(Icons.info), ImageAlignment.right, (value) {
+                  DDLog(value, StackTrace.current);
+                }).decorated(color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(5)),
+                    border: Border.all(width: 1.0, color: Colors.red)),
+
+                TextButtonExt.build("菜单top", Icon(Icons.info), ImageAlignment.top, (value) {
+                  DDLog(value, StackTrace.current);
+                }).decorated(color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(5)),
+                    border: Border.all(width: 1.0, color: Colors.red),),
+
+                TextButtonExt.build("菜单bottom", Icon(Icons.info), ImageAlignment.bottom, (value) {
+                  DDLog(value, StackTrace.current);
+                }).decorated(color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(5)),
+                  border: Border.all(width: 1.0, color: Colors.red),),
+
+              ],
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () => DDLog('floatingActionButton', StackTrace.current),
+            tooltip: 'Increment',
+            child: Icon(Icons.add),
+          ), // This trailing comma makes auto-formatting nicer for build methods.
+        ),
+      ),
+    );
+  }
+
+
+  test() {
+    var _emap = Map<String, List<String>>();
+    // _emap["a"] ??= <String>[],
+    _emap["a"] ??= ["1", "2", "3"];
+
+    var list = _emap["a"];
+    list!.remove("1");
+    // DDLog(list, StackTrace.current);
+    // DDLog(_emap, StackTrace.current);
+
+    DDLog([list, _emap], StackTrace.current);
+  }
+}
---
+++ lib/TabBarDemo.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:styled_widget/styled_widget.dart';
+import 'DDLog.dart';
+import 'FirstPage.dart';
+import 'SecondPage.dart';
+import 'Model/ThirdPage.dart';
+import 'FourthPage.dart';
+import 'BatterLevelPage.dart';
+
+class TabBarDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 5,
+        child: Scaffold(
+          appBar: AppBar(
+            leading: Icon(Icons.arrow_back)
+                    // .gestures(onTap: ()=> DDLog("back", StackTrace.current)
+                      .gestures(onTap: (){ Navigator.pop(context); }),
+            bottom: TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.home)),
+                Tab(icon: Icon(Icons.directions_car)),
+                Tab(icon: Icon(Icons.directions_transit)),
+                Tab(icon: Icon(Icons.directions_bike)),
+                Tab(icon: Icon(Icons.directions_bike)),
+              ],
+            ),
+            title: Text('Tabs Demo'),
+          ),
+          body: TabBarView(
+            children: [
+              FirstPage(title: '',).center(),
+              ThirdPage().center(),
+              SecondPage().center(),
+              FourthPage().center(),
+              BatterLevelPage(title: 'Batter Level Page').center(),
+              // Center(child: SecondPage()),
+              // Center(child: ThirdPage()),
+              // Center(child: FourthPage()),
+
+              // Icon(Icons.home),
+              // Icon(Icons.directions_car),
+              // Icon(Icons.directions_transit),
+              // Icon(Icons.directions_bike),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
