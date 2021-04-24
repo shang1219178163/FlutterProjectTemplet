201fcb87453a1386f6cfe434779e60ab1c77ca86
Shang <shang1219178163@gmail.com>
2021-04-24 10:36:03 +0800

    主页面调整

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertemplet/FirstPage.dart';
+import 'package:fluttertemplet/textlessPage.dart';
 import 'package:styled_widget/styled_widget.dart';
-import 'package:simple_logger/simple_logger.dart';
 import 'package:fluttertemplet/DDLog.dart';
+
+import 'package:fluttertemplet/TabBarDemo.dart';
+
+import 'FButtonPage.dart';
+import 'PopupMenuButton_extension.dart';
 
 void main() {
   runApp(MyApp());
+  // runApp(TabBarDemo());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // This widget is the root of your aptplication.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Templet',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-      ),
+        // brightness: Brightness.dark,//设置明暗模式为暗色
+        // accentColor: Colors.black,//(按钮）Widget前景色为黑色
+        // primaryColor: Colors.cyan,//主色调为青色
+        // iconTheme:IconThemeData(color: Colors.yellow),//设置icon主题色为黄色
+        // textTheme: TextTheme(body1: TextStyle(color: Colors.red))//设置文本颜色为红色
+    ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  MyHomePage({ Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -50,7 +44,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  final logger = SimpleLogger();
 
   void _incrementCounter() {
     setState(() {
@@ -65,36 +58,104 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+
+        //导航条左边的组件
+        leading: Icon(Icons.arrow_back)
+                .opacity(0)
+                .gestures(onTap: ()=> DDLog("back", StackTrace.current)),
+        //导航条右边的一组组件
+        actions: [
+          // Icon(Icons.settings)
+          //     .padding(right: 8)
+          //     .gestures(onTap: ()=> DDLog("settings", StackTrace.current)),
+          // Icon(Icons.search)
+          //     // .padding(right: 8)
+          //     .gestures(onTap: ()=> DDLog("search", StackTrace.current)),
+
+          PopupMenuButtonExt.fromEntryFromJson(
+              json: {"aa": "0",
+                    "bb": "1",
+                    "cc": "2"},
+              checkedString: "aa",
+              callback: (value) {
+                setState(() => DDLog(value, StackTrace.current));
+          }),
+
+          PopupMenuButtonExt.fromCheckList(
+              list: ["a", "b", "c"],
+              checkedIdx: 1,
+              callback: (value) {
+                setState(() => DDLog(value, StackTrace.current));
+          }),
+
+          // PopupMenuButtonExt.from(
+          //     list: ["aaa", "bbb", "ccc"],
+          //     callback: (value) {
+          //       setState(() => DDLog(value, StackTrace.current));
+          //     }),
+          // PopupMenuButtonExt.from(
+          //     list: ["aaa", 'bbb', "ccc"],
+          //     callback: (value) {
+          //       setState(() => DDLog(value, StackTrace.current));
+          //     }),
+          // PopupMenuButtonExt.fromJson(
+          //     json: {"aaaa": "0",
+          //           "cccc": "2",
+          //           "bbbb": "1",},
+          //     callback:(value) {
+          //       setState(() => DDLog(value, StackTrace.current));
+          //     }),
+          // PopupMenuButtonExt.fromJson(
+          //     json: { "aaaa": "0",
+          //         "cccc": "2",
+          //         "bbbb": "1",},
+          //     callback:(value) {
+          //         setState(() => DDLog(value, StackTrace.current));
+          //     }),
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.white,
+        shape: CircularNotchedRectangle(), // 底部导航栏打一个圆形的洞
+        child: Row(
+          children: [
+            IconButton(icon: Icon(Icons.home), highlightColor: Colors.red, onPressed: ()=> DDLog("home", StackTrace.current),
+            ),
+            IconButton(icon: Icon(Icons.directions_car), onPressed: (){
+              Navigator.push(context,
+                MaterialPageRoute(builder: (context) {
+                  return TabBarDemo();
+                }));
+              },
+            ),
+            // IconButton(icon: Icon(Icons.directions_transit), onPressed: ()=> DDLog("directions_transit", StackTrace.current),
+            // ),
+            IconButton(icon: Icon(Icons.directions_car), onPressed: (){
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) {
+                    return TextlessPage(title: "TextlessPage");
+                    // return FButtonPage();
+                  }));
+              },
+            ),
+            IconButton(icon: Icon(Icons.directions_bike),
+              onPressed: ()=> DDLog("directions_bike", StackTrace.current),
+
+            ),
+          ],
+          mainAxisAlignment: MainAxisAlignment.spaceAround, //均分底部导航栏横向空间
+        ),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
+
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
@@ -121,7 +182,8 @@ class _MyHomePageState extends State<MyHomePage> {
             // .gestures(onTap: () => print('${this}_${DateTime.now()} RaisedButton pressed'))
             // .gestures(onTap: () => logger.info('${this}_${DateTime.now()} RaisedButton pressed'))
             // .gestures(onTap: () => print('${DateTime.now()} RaisedButton pressed'))
-                .gestures(onTap: () => DDLog('RaisedButton pressed', StackTrace.current))
+            //  .gestures(onTap: () => DDLog('RaisedButton pressed', StackTrace.current))
+                .gestures(onTap: () => test())
           ],
         ),
       ),
@@ -132,4 +194,20 @@ class _MyHomePageState extends State<MyHomePage> {
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+
+  test(){
+    var a = 85.99999;
+    var b = 488.236;
+    var c = 488.3;
+    
+    DDLog(a.toStringAsExponential(2), StackTrace.current);
+    DDLog(a.toStringAsFixed(2), StackTrace.current);
+    DDLog(a.toStringAsPrecision(2), StackTrace.current);
+    
+    "1111".printNew();
+  }
 }
+
+// class FButtonPage {
+// }
