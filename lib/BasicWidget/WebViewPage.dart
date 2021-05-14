27e7034650ebd4f10b9d0deb51c48c088e7e250a
Shang <shang1219178163@gmail.com>
2021-05-14 22:04:23 +0800

    webview

---
+++ lib/BasicWidget/WebViewPage.dart
@@ -0,0 +1,33 @@
+
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebViewPage extends StatefulWidget{
+  String? title;
+  String url;
+  dynamic params;
+
+  WebViewPage({required this.url, this.params, this.title});
+
+  @override
+  State<StatefulWidget> createState()=>WebViewPageState();
+}
+
+class WebViewPageState extends State<WebViewPage> {
+  @override
+  void initState() {
+    super.initState();
+    // Enable hybrid composition.
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl: 'https://flutter.dev',
+    );
+  }
+}
