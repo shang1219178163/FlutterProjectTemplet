afd443feb13097ac0a7c636677d6bdfa54e3d2b7
Shang <shang1219178163@gmail.com>
2021-05-14 22:03:56 +0800

    新方法扩展

--- lib/DartExpand/Widget_extension.dart
+++ lib/DartExpand/Widget_extension.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertemplet/DartExpand/DDLog.dart';
@@ -65,4 +67,90 @@ extension WidgetExt on Widget {
     animationDuration: animationDuration,
     child: this,
   );
+
+  toShowCupertinoModalPopup({
+    required BuildContext context,
+    ImageFilter? filter,
+    Color barrierColor = kCupertinoModalBarrierColor,
+    bool barrierDismissible = true,
+    bool useRootNavigator = true,
+    bool? semanticsDismissible,
+    RouteSettings? routeSettings,
+  }) =>
+      showCupertinoModalPopup(
+        context: context,
+        builder: (context) => this,
+        filter: filter,
+        barrierColor: barrierColor,
+        barrierDismissible: barrierDismissible,
+        semanticsDismissible: semanticsDismissible,
+        routeSettings: routeSettings,
+      );
+
+
+  /// 时间选择器
+  void showDatePicker({
+    required BuildContext context,
+    DateTime? initialDateTime,
+    required CupertinoDatePickerMode mode,
+    // required void Function(DateTime dateTime, String title) callback}) {
+    required void callback(DateTime dateTime, String title)}) {
+
+    DateTime dateTime = initialDateTime ?? DateTime.now();
+
+    Container(
+      height: 300,
+      // color: Color.fromARGB(255, 255, 255, 255),
+      color: Colors.white,
+      child: Column(
+        children: [
+          Row(children: [
+            // Close the modal
+            CupertinoButton(
+              child: Text('取消'),
+              // onPressed: () => Navigator.of(ctx).pop(),
+              onPressed: (){
+                callback(dateTime, "取消");
+                Navigator.of(context).pop();
+              },
+            ),
+            Expanded(child: Text("请选择",
+              style: TextStyle(fontSize: 17,
+                  fontWeight: FontWeight.normal,
+                  color: Colors.black,
+                  backgroundColor: Colors.white,
+                  decoration: TextDecoration.none),
+              textAlign: TextAlign.center,)
+            ),
+
+            CupertinoButton(
+              child: Text('确定'),
+              // onPressed: () => Navigator.of(ctx).pop(),
+              onPressed: (){
+                callback(dateTime, "确定");
+                Navigator.of(context).pop();
+              },
+            ),
+          ],),
+          Container(
+            height: 216,
+            color: Colors.white,
+            child: CupertinoDatePicker(
+                mode: mode,
+                initialDateTime: initialDateTime,
+                onDateTimeChanged: (val) {
+                    dateTime = val;
+                    DDLog(val);
+                }),
+          ),
+        ],
+      ),
+    ).toShowCupertinoModalPopup(context: context);
+  }
 }
+
+extension DateTimeExt on DateTime {
+  String toString19() => this.toString().split(".").first;
+
+}
+
