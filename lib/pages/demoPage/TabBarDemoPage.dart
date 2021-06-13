d431ee8d632eb11fdb4dcbdfa6d7a0eee0ed0cf9
Shang <shang1219178163@gmail.com>
2021-06-13 11:27:37 +0800

    优化

--- lib/pages/demoPage/TabBarDemoPage.dart
+++ lib/pages/demoPage/TabBarDemoPage.dart
@@ -6,9 +6,30 @@ import 'package:fluttertemplet/pages/ThirdPage.dart';
 import 'package:fluttertemplet/pages/FourthPage.dart';
 import 'package:fluttertemplet/pages/BatterLevelPage.dart';
 
-class TabBarDemo extends StatelessWidget {
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class TabBarDemoPage extends StatefulWidget {
+
+  final String? title;
+
+  TabBarDemoPage({ Key? key, this.title}) : super(key: key);
+
+  @override
+  _TabBarDemoPageState createState() => _TabBarDemoPageState();
+}
+
+class _TabBarDemoPageState extends State<TabBarDemoPage> with AutomaticKeepAliveClientMixin{
+
+  @override
+  bool get wantKeepAlive => true;
+
+
   @override
   Widget build(BuildContext context) {
+    super.build(context);//必须添加
+
     return MaterialApp(
       home: DefaultTabController(
         length: 5,
@@ -19,30 +40,25 @@ class TabBarDemo extends StatelessWidget {
             //           .gestures(onTap: (){ Navigator.pop(context); }),
             bottom: TabBar(
               tabs: [
-                Tab(icon: Icon(Icons.home)),
+                Tab(icon: Icon(Icons.directions_railway)),
                 Tab(icon: Icon(Icons.directions_car)),
-                Tab(icon: Icon(Icons.directions_transit)),
+                Tab(icon: Icon(Icons.directions_bus)),
                 Tab(icon: Icon(Icons.directions_bike)),
-                Tab(icon: Icon(Icons.directions_bike)),
+                Tab(icon: Icon(Icons.directions_boat)),
               ],
             ),
             title: Text('Tabs Demo'),
           ),
           body: TabBarView(
             children: [
-              FirstPage(title: '',).center(),
+              FirstPage().center(),
               SecondPage().center(),
               ThirdPage().center(),
               FourthPage().center(),
-              BatterLevelPage(title: 'Batter Level Page').center(),
+              BatterLevelPage().center(),
               // Center(child: SecondPage()),
               // Center(child: ThirdPage()),
               // Center(child: FourthPage()),
-
-              // Icon(Icons.home),
-              // Icon(Icons.directions_car),
-              // Icon(Icons.directions_transit),
-              // Icon(Icons.directions_bike),
             ],
           ),
         ),
