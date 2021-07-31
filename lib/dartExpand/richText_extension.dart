716f2dde3cf809250c438b93890ce54207720446
Shang <shang1219178163@gmail.com>
2021-07-31 14:03:18 +0800

    富文本扩展文件

---
+++ lib/dartExpand/richText_extension.dart
@@ -0,0 +1,45 @@
+//
+//  richText_extension.dart
+//  fluttertemplet
+//
+//  Created by shang on 7/31/21 1:48 PM.
+//  Copyright © 7/31/21 shang. All rights reserved.
+//
+
+
+
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+
+extension RichTextExt on RichText{
+  /// List<TextSpan> by [String text], [Map<String, String> linkMap]
+  static List<TextSpan> createTextSpans(BuildContext context, {required String text, required Map<String, String> linkMap, TextStyle? style, TextStyle? linkStyle, required void onTap(String key, String value)}) {
+    assert(text.isNotEmpty);
+    assert(linkMap.isNotEmpty);
+    linkMap.forEach((key, value) {
+      assert(text.contains(key));
+    });
+
+    final titles = linkMap.keys;
+    final key = titles.join("|");
+    // ddlog(key);
+    final list = text.split(RegExp('《|》'));
+    // ddlog(list.length);
+
+    List<TextSpan> textSpans = list
+        .map((e) => !titles.contains("《$e》")
+        ? TextSpan(text: e, style: style)
+        : TextSpan(
+      text: "《$e》",
+      style: linkStyle ?? TextStyle(color: Theme.of(context).primaryColor),
+      // style: TextStyle(color: Colors.blue),
+      recognizer: TapGestureRecognizer()
+        ..onTap = () {
+            onTap("《$e》", linkMap["《$e》"] ?? "");
+        },
+    ))
+        .toList();
+    return textSpans;
+  }
+}
