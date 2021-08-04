59c4478be78c50bc459fb775c4ca33d458edc428
Shang <shang1219178163@gmail.com>
2021-08-04 15:47:32 +0800

    initail

---
+++ lib/dartExpand/string_extension.dart
@@ -0,0 +1,18 @@
+//
+//  string_extension.dart
+//  fluttertemplet
+//
+//  Created by shang on 8/3/21 11:41 AM.
+//  Copyright © 8/3/21 shang. All rights reserved.
+//
+
+
+extension StringExt on String{
+  /// 获取匹配到的元素数组
+  List<String> allMatchesByReg(RegExp regExp) {
+    final reg = regExp.allMatches(this);
+    final list = reg.map((e) => e.group(0)).where((e) => e != null).toList();
+    final List<String> titles = list.whereType<String>().toList();
+    return titles;
+  }
+}
