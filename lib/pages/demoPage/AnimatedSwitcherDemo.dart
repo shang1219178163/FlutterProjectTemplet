257f87d6afdcade588a23373a07fd623d5ff048f
Shang <shang1219178163@gmail.com>
2021-10-19 22:43:20 +0800

    initail

---
+++ lib/pages/demoPage/AnimatedSwitcherDemo.dart
@@ -0,0 +1,148 @@
+//
+//  AnimatedSwitcherDemo.dart
+//  fluttertemplet
+//
+//  Created by shang on 10/19/21 10:08 PM.
+//  Copyright © 10/19/21 shang. All rights reserved.
+//
+
+import 'package:flutter/material.dart';
+
+class AnimatedSwitcherDemo extends StatefulWidget {
+
+  final String? title;
+
+  AnimatedSwitcherDemo({ Key? key, this.title}) : super(key: key);
+
+  
+  @override
+  _AnimatedSwitcherDemoState createState() => _AnimatedSwitcherDemoState();
+}
+
+class _AnimatedSwitcherDemoState extends State<AnimatedSwitcherDemo> {
+  int _count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    dynamic arguments = ModalRoute.of(context)!.settings.arguments;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title ?? "$widget"),
+      ),
+      body: buildBody(context),
+    );
+  }
+
+  Widget buildBody(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          // AnimatedSwitcher(
+          //   duration: const Duration(milliseconds: 500),
+          //   transitionBuilder: (Widget child, Animation<double> animation) {
+          //     //执行缩放动画
+          //     return ScaleTransition(child: child, scale: animation);
+          //   },
+          //   child: Text(
+          //     '$_count',
+          //     //显示指定key，不同的key会被认为是不同的Text，这样才能执行动画
+          //     key: ValueKey<int>(_count),
+          //     style: Theme.of(context).textTheme.headline4,
+          //   ),
+          // ),
+          AnimatedSwitcher(
+            duration: Duration(milliseconds: 200),
+            transitionBuilder: (Widget child, Animation<double> animation) {
+              var tween=Tween<Offset>(begin: Offset(1, 0), end: Offset(0, 0));
+              return SlideTransitionX(
+                child: child,
+                direction: AxisDirection.down, //上入下出
+                position: animation,
+              );
+            },
+          ),
+          RaisedButton(
+            child: const Text('+1',),
+            onPressed: () {
+              setState(() {
+                _count += 1;
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+///移动动画
+class SlideTransitionX extends AnimatedWidget {
+  SlideTransitionX({
+    Key? key,
+    required Animation<double> position,
+    this.transformHitTests = true,
+    this.direction = AxisDirection.down,
+    required this.child,
+  })
+      : assert(position != null),
+        super(key: key, listenable: position) {
+    // 偏移在内部处理
+    switch (direction) {
+      case AxisDirection.up:
+        _tween = Tween(begin: Offset(0, 1), end: Offset(0, 0));
+        break;
+      case AxisDirection.right:
+        _tween = Tween(begin: Offset(-1, 0), end: Offset(0, 0));
+        break;
+      case AxisDirection.down:
+        _tween = Tween(begin: Offset(0, -1), end: Offset(0, 0));
+        break;
+      case AxisDirection.left:
+        _tween = Tween(begin: Offset(1, 0), end: Offset(0, 0));
+        break;
+    }
+  }
+
+
+  Animation<double> get position => listenable;
+
+
+  final bool transformHitTests;
+
+  final Widget child;
+
+  //退场（出）方向
+  final AxisDirection direction;
+
+  late final Tween<Offset> _tween;
+
+  @override
+  Widget build(BuildContext context) {
+    Offset offset = _tween.evaluate(position!);
+    if (position.status == AnimationStatus.reverse) {
+      switch (direction) {
+        case AxisDirection.up:
+          offset = Offset(offset.dx, -offset.dy);
+          break;
+        case AxisDirection.right:
+          offset = Offset(-offset.dx, offset.dy);
+          break;
+        case AxisDirection.down:
+          offset = Offset(offset.dx, -offset.dy);
+          break;
+        case AxisDirection.left:
+          offset = Offset(-offset.dx, offset.dy);
+          break;
+      }
+    }
+    return FractionalTranslation(
+      translation: offset,
+      transformHitTests: transformHitTests,
+      child: child,
+    );
+  }
+}
